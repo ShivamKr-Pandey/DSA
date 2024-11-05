eef0998dcca051eba9edd586e5abd8cb73d5ae24
Shivam Pandey <shivam.kumarpandey@growthnatives.com>
2024-11-05 15:02:37 +0530

    Removed unwanted files

---
+++ Patterns/patterns.py
@@ -0,0 +1,15 @@
+################ Patterns Questions ################
+
+######### Pattern 1 #########
+
+rows = input("Enter a number:")
+
+converted_integer = int(rows)
+
+for i in range(converted_integer):    
+    for x in range(converted_integer):
+        print("* ", end="")
+    print()
+
+
+######### Pattern 2 ######### 
