d800b00fd6bebe689ebb05efb41e8ba267273e70
Shivam Pandey <itsshivampandey@gmail.com>
2024-12-02 19:19:43 +0530

    Done till yuple

--- Data Types/lists.py
+++ Data Types/lists.py
@@ -1,3 +1,20 @@
 x = ["Now", "we", "are", "cooking!"]
-type(x)
+print(type(x))
 print(x, end=" ")
+len(x)
+
+
+fruits = ["Pineapple", "Apple", "Banana", "Mango"]
+
+# Append
+fruits.append("Kiwi")
+
+# Insert
+fruits.insert(1,"Orange")
+
+# Remove
+fruits.remove("Apple")
+
+# Pop
+fruits.pop(4) 
+print(fruits)
---
+++ Data Types/tuple.py
@@ -0,0 +1,21 @@
+fullname = ("Shivam", "Kumar", "Pandey")
+print(type(fullname))
+
+def converted_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds - hours * 3600) // 60
+    remaining_seconds = seconds - hours * 3600 - minutes * 60
+    return hours, minutes, remaining_seconds
+
+result = converted_seconds(5000)
+print(type(result))
+print(result)
+
+# Unpacking the Tuple
+
+hours, minutes , seconds = result
+print(hours, minutes, seconds)
+
+hours, minutes, seconds = converted_seconds(2000)
+print(hours, minutes, seconds)
+
