355fbd58ec021caeee192e942f472f8478e0a941
Shivam Pandey <itsshivampandey@gmail.com>
2024-12-12 16:46:22 +0530

    Parts of string

--- Data Types/strings.py
+++ Data Types/strings.py
@@ -1,2 +1,21 @@
 name = "Sasha"
-print(name)
+# print(name)
+
+
+# Parts of String
+# print(name[2]) 
+
+text = "Randow string with lot of words"
+
+# print(text[-2])
+
+# Slice
+
+color = "Orange"
+
+# print(color[1:4])
+
+fruit = "Pineapple"
+
+print(fruit[:4])
+print(fruit[3:])
